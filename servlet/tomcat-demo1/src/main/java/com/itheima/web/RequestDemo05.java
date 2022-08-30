9b297857ffed0e75cf2d9f149d3aaa03b0e59132
wangchen <wangchen@bonree.com>
2022-08-30 09:55:17 +0800

    编码类型 以及请求转发

---
+++ servlet/tomcat-demo1/src/main/java/com/itheima/web/RequestDemo05.java
@@ -0,0 +1,21 @@
+package com.itheima.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/req5")
+public class RequestDemo04 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req,resp);
+    }
+}
