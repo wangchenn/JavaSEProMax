b2e6cea490e0f55299fb5b3fb580080376bec3ba
wangchen <wangchen@bonree.com>
2022-09-02 15:58:22 +0800

    Response练习

---
+++ servlet/tomcat-demo1/src/main/java/com/itheima/web/response/ResponseDemo1.java
@@ -0,0 +1,2 @@
+package com.itheima.web.response;public class ResponseDemo1 {
+}
---
+++ servlet/tomcat-demo1/src/main/java/com/itheima/web/response/ResponseDemo4.java
@@ -0,0 +1,29 @@
+package com.itheima.web.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/resp3")
+public class ResponseDemo3 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        //获取字符输出流
+        PrintWriter printWriter = resp.getWriter();
+        //content-type 设置解析格式
+        resp.setHeader("content-type","text/html");
+        printWriter.write("aaa");
+        printWriter.write("你好,中国");
+        printWriter.write("<h1>aaa</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req,resp);
+    }
+}
